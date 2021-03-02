af7c8c4191626e7e728164c8eeda1038be36602f
uryuryu <79921833+uryuryu@users.noreply.github.com>
2021-03-03 00:12:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,25 +1,8 @@
-from discord.ext import commands
-import os
-import traceback
+# インストールした discord.py を読み込む
+import discord
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-bot.run(token)
-
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = 'ODE1OTA5NTMxMjgwMzQzMTAw.YDzQ9w.40j4jgR2RpFohOMvLwuKdxmLiNE'
 
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
